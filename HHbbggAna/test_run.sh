352c42ca0fe065234839c8b1f3a2dfe49c1f5470
Nan Lu <nan.lu@cern.ch>
2021-06-24 18:28:09 -0700

    update test command

--- HHbbggAna/test_run.sh
+++ HHbbggAna/test_run.sh
@@ -2,6 +2,6 @@ for dataset in `cat testfile/bbgg_nanoAOD_file_lists.txt`; do
     echo "get a list of dataset:", $dataset
 
     ./analyzeHHbbgg testfile/lists/$dataset.txt $dataset.root $dataset F F 2018
-    #./analyzeHHbbgg runList.txt out.root mc F F 2018
+    #./analyzeHHbbgg runList.txt out.root GluGluToHHTo2B2G_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8 F T 2018
 
 done
